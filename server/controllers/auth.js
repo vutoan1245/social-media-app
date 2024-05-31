e86a34a005f1a5218d8fa5d6465a9b6213f3c5e0
Toan Vu <vutoan1245@gmail.com>
2024-05-30 23:12:53 -0400

    Delete time limit

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -54,9 +54,7 @@ export const login = async (req, res) => {
       return res.status(400).json({ message: "Invalid credentials." });
     }
 
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-      expiresIn: "1h",
-    });
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     const userResponse = { ...user._doc };
     delete userResponse.password;
 
