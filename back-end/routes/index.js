4a8aca6698aaaf071eec01c2e2c191e50ec4527f
Toan Vu <vutoan1245@gmail.com>
2024-06-07 20:12:05 -0400

    refactor: Add search api endpoint for posts and users

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -3,9 +3,11 @@ import authRoutes from "./authRoutes.js";
 import postRoutes from "./postRoutes.js";
 import commentRoutes from "./commentRoutes.js";
 import userRoutes from "./userRoutes.js";
+import searchRoutes from "./searchRoutes.js";
 
 const router = express.Router();
 
+router.use(searchRoutes);
 router.use(authRoutes);
 router.use(postRoutes);
 router.use(commentRoutes);
---
+++ back-end/routes/searchRoutes.js
@@ -0,0 +1,36 @@
+import express from "express";
+import Post from "../models/Post.js";
+import User from "../models/User.js";
+import { verifyToken } from "../middleware/authMiddleware.js";
+
+const router = express.Router();
+
+// Search for posts and users
+router.get("/search", verifyToken, async (req, res) => {
+  const { query } = req.query;
+
+  if (!query) {
+    return res.status(400).json({ error: "Query parameter is required" });
+  }
+
+  try {
+    // Search for posts that match the query
+    const posts = await Post.find({
+      content: { $regex: query, $options: "i" },
+    }).populate("userId", "firstName lastName picturePath");
+
+    // Search for users that match the query
+    const users = await User.find({
+      $or: [
+        { firstName: { $regex: query, $options: "i" } },
+        { lastName: { $regex: query, $options: "i" } },
+      ],
+    }).select("-password -email");
+
+    res.status(200).json({ posts, users });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+export default router;
