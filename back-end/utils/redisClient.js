73622601eef1436be78968efbfaf19ea4945b6d7
Toan Vu <vutoan1245@gmail.com>
2024-06-09 12:16:41 -0400

    refactor: Update Redis configuration to use environment variables for host and port

--- back-end/utils/redisClient.js
+++ back-end/utils/redisClient.js
@@ -1,8 +1,8 @@
 import Redis from "ioredis";
 
 const redisClient = new Redis({
-  host: "localhost",
-  port: 6379,
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
 });
 
 redisClient.on("error", (err) => console.error("Redis error:", err));
