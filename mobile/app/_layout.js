ff4a5ec312b51f687c0befee7522cc228b1de3e1
Toan Vu <vutoan1245@gmail.com>
2024-06-27 12:13:48 -0400

    feat: Add @reduxjs/toolkit and react-redux dependencies

--- mobile/app/_layout.js
+++ mobile/app/_layout.js
@@ -1,41 +1,44 @@
-import { View, Text, Button } from "react-native";
-import React from "react";
+import { Button } from "react-native";
+import { Provider } from "react-redux";
 import { Stack, router } from "expo-router";
+import store from "../store/store";
 
 export default function _layout() {
   return (
-    <Stack
-      screenOptions={{
-        headerStyle: {
-          backgroundColor: "red",
-        },
-        headerTintColor: "white",
-      }}
-    >
-      <Stack.Screen
-        name="index"
-        options={{
-          headerTitle: "Home",
-          headerRight: () => (
-            <Button
-              onPress={() => {
-                router.push("contact");
-              }}
-              title="Contact"
-            />
-          ),
+    <Provider store={store}>
+      <Stack
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "red",
+          },
+          headerTintColor: "white",
         }}
-      />
-      <Stack.Screen name="about" options={{ headerTitle: "About" }} />
-      <Stack.Screen
-        name="blog/index"
-        options={{ headerTitle: "All Blog Posts" }}
-      />
-      <Stack.Screen
-        name="contact"
-        options={{ headerTitle: "Contact", presentation: "modal" }}
-      />
-      <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
-    </Stack>
+      >
+        <Stack.Screen
+          name="index"
+          options={{
+            headerTitle: "Home",
+            headerRight: () => (
+              <Button
+                onPress={() => {
+                  router.push("contact");
+                }}
+                title="Contact"
+              />
+            ),
+          }}
+        />
+        <Stack.Screen name="about" options={{ headerTitle: "About" }} />
+        <Stack.Screen
+          name="blog/index"
+          options={{ headerTitle: "All Blog Posts" }}
+        />
+        <Stack.Screen
+          name="contact"
+          options={{ headerTitle: "Contact", presentation: "modal" }}
+        />
+        <Stack.Screen name="(drawer)" options={{ headerShown: false }} />
+      </Stack>
+    </Provider>
   );
 }
