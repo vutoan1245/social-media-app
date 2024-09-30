aba77239f187732186f130d3a32b47624a218424
Toan Vu <vutoan1245@gmail.com>
2024-09-30 16:12:43 -0400

    fix: add res parameter

--- back-end/index.js
+++ back-end/index.js
@@ -38,7 +38,7 @@ app.use(
   express.static(path.join(__dirname, "public/assets"), {})
 );
 
-app.get('/', () => {
+app.get('/', (req, res) => {
   res.status(200).json({ message: 'Working!!!!' });
 });
 
